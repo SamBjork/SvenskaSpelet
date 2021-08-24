73df0b3738d151af16d76723e17fb2314d69bb0f
Sam <samuel.bjorkqvist@gmail.com>
2021-08-24 12:24:21 +0200

    refacting

--- Backend/main.py
+++ Backend/main.py
@@ -18,84 +18,66 @@ class HelloWorld(Resource):
         return{'data': 'Hello World'}
 
 
-class PostUser(Resource):
-    def post(self):
-        mariadb_connection = mariadb.connect(
-            user='root', password='secret', host='127.0.0.1', port=3306, database='SvenskaSpelet')
-        mycursor = mariadb_connection.cursor()
-        #create_cursor.execute('Select * From users')
-        sql = "INSERT INTO users (username, password) VALUES (%s, %s)"
-        val = ("John", "Highway 21")
-        mycursor.execute(sql, val)
-        mariadb_connection.commit()
-        print(mycursor, "record inserted.")
-
-        return ("record inserted")
-
-
-class UpdateUser(Resource):
-    def put(self):
-        mariadb_connection = mariadb.connect(
-            user='root', password='secret', host='127.0.0.1', port=3306, database='SvenskaSpelet')
-        mycursor = mariadb_connection.cursor()
-        #create_cursor.execute('Select * From users')
-        sql = "UPDATE users SET username = 'first' WHERE id = 1"
-        mycursor.execute(sql)
-        mariadb_connection.commit()
-        print(mycursor, "record inserted.")
-
-        return ("record inserted")
-
-
-class GetUsers(Resource):
-    def get(self):
-        mariadb_connection = mariadb.connect(
-            user='root', password='secret', host='127.0.0.1', port=3306, database='SvenskaSpelet')
-        create_cursor = mariadb_connection.cursor()
-        create_cursor.execute('Select * From users')
-
-        myresult = create_cursor.fetchall()
-        # return
-
-        return "{}".format(myresult)
-
-
-class DeleteUser(Resource):
-    def delete(self):
-        mariadb_connection = mariadb.connect(
-            user='root', password='secret', host='127.0.0.1', port=3306, database='SvenskaSpelet')
-        mycursor = mariadb_connection.cursor()
-        sql = "DELETE FROM users WHERE id = '1'"
-
-        mycursor.execute(sql)
-
-        mariadb_connection.commit()
-
-        print(mycursor.rowcount, "record(s) deleted")
-        return(mycursor.rowcount, "record(s) deleted")
-
-
 api.add_resource(HelloWorld, "/hw")
 
-api.add_resource(PostUser, "/pu")
-
-api.add_resource(GetUsers, "/gu")
-
-api.add_resource(UpdateUser, "/uu")
-
-api.add_resource(DeleteUser, "/du")
+# User
 
 
-@app.route('/db')
-def database():
-    config = {'host': '127.0.0.1:5000', 'port': 3306, 'user': 'root',
-              'password': 'secret', 'database': 'SvenskaSpelet'}
-    conn = mariadb.connect(**config)
-    cur = conn.cursor()
-    sql = "Select * From users"
-    cur.execute(sql)
-    myresult = cur.fetchall()
+@app.route('/pu')
+def post(self):
+    mariadb_connection = mariadb.connect(
+        user='root', password='secret', host='127.0.0.1', port=3306, database='SvenskaSpelet')
+    mycursor = mariadb_connection.cursor()
+    #create_cursor.execute('Select * From users')
+    sql = "INSERT INTO users (username, password) VALUES (%s, %s)"
+    val = ("John", "Highway 21")
+    mycursor.execute(sql, val)
+    mariadb_connection.commit()
+    print(mycursor, "record inserted.")
+
+    return ("record inserted")
+
+
+@app.route('/uu')
+def put(self):
+    mariadb_connection = mariadb.connect(
+        user='root', password='secret', host='127.0.0.1', port=3306, database='SvenskaSpelet')
+    mycursor = mariadb_connection.cursor()
+    #create_cursor.execute('Select * From users')
+    sql = "UPDATE users SET username = 'first' WHERE id = 1"
+    mycursor.execute(sql)
+    mariadb_connection.commit()
+    print(mycursor, "record inserted.")
+
+    return ("record inserted")
+
+
+@app.route('/gu')
+def get(self):
+    mariadb_connection = mariadb.connect(
+        user='root', password='secret', host='127.0.0.1', port=3306, database='SvenskaSpelet')
+    create_cursor = mariadb_connection.cursor()
+    create_cursor.execute('Select * From users')
+
+    myresult = create_cursor.fetchall()
+    # return
+
     return "{}".format(myresult)
+
+
+@app.route('/du')
+def delete_user(self):
+    mariadb_connection = mariadb.connect(
+        user='root', password='secret', host='127.0.0.1', port=3306, database='SvenskaSpelet')
+    mycursor = mariadb_connection.cursor()
+    sql = "DELETE FROM users WHERE id = '1'"
+
+    mycursor.execute(sql)
+
+    mariadb_connection.commit()
+
+    print(mycursor.rowcount, "record(s) deleted")
+    return(mycursor.rowcount, "record(s) deleted")
 
 
 if __name__ == "__main__":
